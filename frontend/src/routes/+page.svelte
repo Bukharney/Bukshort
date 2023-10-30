22847245376d4f18954fb0db06c32a6b3566892b
Jirapat <bukharney@gmail.com>
2023-10-30 23:13:28 +0700

    change to usable

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,47 +1,95 @@
 <script lang="ts">
-    let originalURL = "";
-    let shortURL = "";
-  
-    function shortenHandler() {
-      fetch("https://shorterurl-production-039e.up.railway.app/shorten", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          url: originalURL
-        })
-      }).then(res => res.json()).then(data => {
+  let originalURL = "";
+  let shortURL = "";
+
+  function shortenHandler() {
+    fetch("https://shorterurl-production-039e.up.railway.app/shorten", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        url: originalURL,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
         console.log(data);
-        shortURL = "https://shorterurl-production-039e.up.railway.app/"+data.url;
+        shortURL = data.url;
       });
-    }
-  </script>
-  
-  <main>
-    <label for="url">URL</label>
-    <input id="url" type="text" placeholder="https://example.com" bind:value={originalURL}/>
-    <button on:click={
-      shortenHandler
-    }>Shorten</button>
-  
-    {#if shortURL !== ""}
-      <p>
-        {shortURL}
-        <button on:click={
-          () => {
-            window.location.href = shortURL;
-          }
-        }>Go to this link</button>
-        <button on:click={
-          () => {
-            navigator.clipboard.writeText(shortURL);
-          }
-        }>Copy</button>
-      </p>
-      
-    {/if}
-  </main>
-  
-  <style>
-  </style>
+  }
+</script>
+
+<div class="bg-white">
+  <div class="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
+    <div
+      class="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:px-24 lg:pt-0"
+    >
+      <svg
+        viewBox="0 0 1024 1024"
+        class="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0"
+        aria-hidden="true"
+      >
+        <circle
+          cx="512"
+          cy="512"
+          r="512"
+          fill="url(#759c1415-0410-454c-8f7c-9a820de03641)"
+          fill-opacity="0.7"
+        />
+        <defs>
+          <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
+            <stop stop-color="#7775D6" />
+            <stop offset="1" stop-color="#E935C1" />
+          </radialGradient>
+        </defs>
+      </svg>
+      <div class="relative h-full w-full text-white">
+        <div class="grid-cols-flow grid-rows-flow grid gap-2 py-2">
+          <label for="url">URL</label>
+          <input
+            class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+            id="url"
+            type="text"
+            placeholder="https://example.com"
+            bind:value={originalURL}
+          />
+          <button
+            class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+            on:click={shortenHandler}>Shorten</button
+          >
+        </div>
+
+        {#if shortURL !== ""}
+          <div class="grid-cols-flow grid-rows-flow grid gap-2 py-2">
+            <div
+              class="text-white block w-full rounded-md border-0 py-1.5 pl-7 pr-20 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            >
+              {shortURL}
+            </div>
+            <button
+              class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+              on:click={() => {
+                window.location.href =
+                  "https://shorterurl-production-039e.up.railway.app/" +
+                  shortURL;
+              }}>Go to this link</button
+            >
+            <button
+              class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+              on:click={() => {
+                navigator.clipboard.writeText(
+                  "https://shorterurl-production-039e.up.railway.app/" +
+                    shortURL
+                );
+              }}>Copy</button
+            >
+          </div>
+        {/if}
+      </div>
+    </div>
+  </div>
+</div>
+
+<style>
+</style>
