d5b4be8271cec23c83f9e0e76be6f2941e9ec692
Jirapat <bukharney@gmail.com>
2023-10-30 20:55:19 +0700

    change cors

--- backend/main.go
+++ backend/main.go
@@ -50,7 +50,7 @@ func main() {
 
 	r.Use(
 		cors.New(cors.Config{
-			AllowOrigins: []string{"https://railway.app/project/f22076ea-5cc1-4c6e-97fd-d8b6c3ce55d9"},
+			AllowOrigins: []string{"https://shorter-l7vhqwz52-bukharney.vercel.app/"},
 			AllowMethods: []string{"GET", "POST"},
 		}),
 	)
